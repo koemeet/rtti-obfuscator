651d14524ef5ce080507707808025ac7cfea343a
Steffen Brem <steffenbrem@gmail.com>
2017-05-03 12:40:30 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <random>
+#include <time.h>
+#include <cstdlib>
+
+#include <boost/regex.hpp>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+
+std::vector<std::string> usedTypeNames;
+
+std::string getRandomString(const int len) {
+	static const char alpha[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+
+	static const char alphanum[] =
+		"0123456789"
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz";
+
+	string s;
+	s.resize(len);
+
+	for (auto i = 0; i < len; ++i) {
+		// we always want to start types with an alphabetical letter
+		if (0 == i)
+		{
+			s[i] = alpha[rand() % (sizeof(alpha) - 1)];
+
+		}
+		else
+		{
+			s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];	
+		}
+	}
+
+	s[len] = 0;
+	
+	return s;
+}
+
+int main(int argc, char* argv[])
+{
+	cout << "===== RTTI obfuscator by koemeet =====" << endl;
+
+	srand(time(nullptr));
+
+	try 
+	{
+		if (argc < 2) 
+		{
+			throw std::exception("Please provide a path or drop a file into me.");
+		}
+
+		// path to input binary
+		string path = argv[1];
+
+		std::ifstream fs(path, std::fstream::binary);
+		if (fs.fail())
+		{
+			throw std::exception("Could not open source binary");
+		}
+
+		// read file contents
+		std::stringstream ss;
+		ss << fs.rdbuf();
+		auto contents = ss.str();
+
+		boost::regex reg("\\.(\\?AV|PEAV)(.+?)@@");
+
+		// replace RTTI types
+		contents = boost::regex_replace(contents, reg, [](const boost::smatch& m)
+		{
+			auto prefix = m[1].str();
+			auto typeName = m[2].str();
+
+			reverse(typeName.begin(), typeName.end());
+
+			return "." + prefix + getRandomString(typeName.size()) + "@@";
+		});
+
+		// generate output path
+		boost::filesystem::path p(path);
+		auto outputPath = p.parent_path().string() + "\\" + p.stem().string() + ".tan" + p.extension().string();
+
+		// write to file
+		std::ofstream os(outputPath, std::ofstream::trunc | std::ofstream::binary);
+		if (!os.write(contents.data(), contents.size()))
+		{
+			throw std::exception((std::string("Unable to write to file ") + outputPath).c_str());
+		}
+		else
+		{
+			cout << "Successfully obfuscated RTTI information. Output written to " << outputPath << endl;
+			system("pause");
+		}
+	} 
+	catch (std::exception &e)
+	{
+		cout << e.what() << endl;
+		system("pause");
+		return 1;
+	}
+
+    return 0;
+}
